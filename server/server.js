68c671b7355c506b4eb8c6e47f5cea4a8d2df1bd
Dipak Shaaki <shanki.dipak@gmail.com>
2025-05-27 12:38:40 +0545

    Database added

--- server/server.js
+++ server/server.js
@@ -3,15 +3,25 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
-// dotenv.config();
-// connectDB();
+// Load env variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
-// app.use(cors());
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
-// app.use('/api/auth', require('./routes/authRoutes'));
-// app.use('/api/notes', require('./routes/noteRoutes'));
+// Simple route to test server
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
